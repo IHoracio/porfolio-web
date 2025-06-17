89f46c6c0c7624327d141c223d3ee42a2b9fa59b
Mario <mariomartinmorilla@gmail.com>
2025-06-17 19:43:10 +0200

    shadow efect between header and body

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,7 +3,7 @@ import '../../styles/index.css';
 function Header() {
 
   return (
-    <div className="bg-gray-700 p-5 flex justify-between items-center">
+    <div className="bg-gray-700 p-5 flex justify-between items-center shadow-lg">
       <span className="text-blue-100 md:ml-15">mariomartinmorilla.es</span>
       <div className="mr-2 md:mr-15">
         <button
