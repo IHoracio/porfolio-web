0d73ab59ae61162d33246b7f91602493a7e522c7
Mario <mariomartinmorilla@gmail.com>
2025-06-21 12:26:55 +0200

    Footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,6 +1,15 @@
 import '../../styles/index.css';
 
 function Footer(){
+    const currentYear = new Date().getFullYear();
 
+return (
+    <>
+    <div className="bg-gray-700 p-5 flex justify-between items-center shadow-lg">
+        <span className="text-blue-100 md:ml-15">mariomartinmorilla.es</span>
+        <span>© {currentYear} Mario Martin Morilla</span>
+    </div>
+    </>
+  );
 }
 export default Footer
